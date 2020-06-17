626f5a91f5388af14c2066633a66e48cc48b3bf8
colbyfayock <fayock@gmail.com>
2020-06-17 16:36:02 -0400

    adding input

--- src/action.js
+++ src/action.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
+const core = require('@actions/core');
 
 async function run() {
+  const character = core.getInput('character');
+
+  console.log(character);
+
   const response = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1');
   const { data = [] } = response;
   const firstEntry = data[0];
