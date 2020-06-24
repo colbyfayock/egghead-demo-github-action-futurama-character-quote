0fc69fbf19585cf5a0f62d6de61b86df90b89bc9
colbyfayock <fayock@gmail.com>
2020-06-24 15:07:37 -0400

    debug logging

--- src/action.js
+++ src/action.js
@@ -13,13 +13,22 @@ const AVAILABLE_CHARACTERS = [
 async function run() {
   const character = core.getInput('character') || DEFAULT_CHARACTER;
 
+  core.debug(`[Futurama] Input Character: ${character}`)
+
   if ( !AVAILABLE_CHARACTERS.includes(character) ) {
     core.setFailed(`Unknown character: ${character}`)
     return;
   }
 
+  core.debug(`[Futurama] Retrieving quote for: ${character}`);
+
   const response = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${character}/1`);
+  core.debug(`[Futurama] Successfully retrieved quote for: ${character}`)
+
   const { data = [] } = response;
+
+  core.debug(`[Futurama] Data: ${JSON.stringify(data)}`)
+
   const firstEntry = data[0];
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
 
